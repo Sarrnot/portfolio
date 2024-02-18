33806d014bd9700b28e2a5176372484c4bb26a79
Sarrnot <urassus@gmail.com>
2024-02-18 17:34:25 +0100

    arcade: add game over stage content

--- web/app/arcade/_assets/Arcade/Stage/GameOverStage.tsx
+++ web/app/arcade/_assets/Arcade/Stage/GameOverStage.tsx
@@ -1,5 +1,21 @@
+import { setStage } from "../Store/Data/stageSlice";
+import { useArcadeDispatch, useArcadeSelector } from "../Store/hooks";
+
 const GameOverStage = () => {
-    return <></>;
+    const dispatch = useArcadeDispatch();
+    const score = useArcadeSelector((state) => state.gameData.score);
+
+    return (
+        <div
+            className="flex flex-col justify-center items-center text-center gap-6 h-full"
+            onClick={() => dispatch(setStage("newGame"))}
+        >
+            <div className="text-3xl">
+                Your final score is: <span className="font-bold">{score}</span>
+            </div>
+            <div>Click anywhere to continue</div>
+        </div>
+    );
 };
 
 export default GameOverStage;
