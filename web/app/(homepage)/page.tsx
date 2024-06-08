15c29b966dcc6d7cd2fa76f0c4769f8dd983bd5a
Sarrnot <sarrnot@sarrnot.com>
2024-06-08 16:17:50 +0200

    add title and repeating background

--- web/app/(homepage)/page.tsx
+++ web/app/(homepage)/page.tsx
@@ -5,27 +5,33 @@ import { useEffect, useRef } from "react";
 
 const HomePage = () => {
     return (
-        <main className="bg-red-200">
-            <section className="h-[1000px] overflow-hidden relative">
-                <>
-                    <Test name="sky" speed={0} />
-                    <Test name="sun" speed={0} />
-                    <Test name="mountains_back" speed={20} />
-                    <Test name="mountains" speed={30} />
-                    <Test name="forest" speed={75} />
-                    <Test name="land" speed={85} />
-                    <Test name="water" speed={100} />
-                    <Test name="boat" speed={100} />
-                    <Test name="reflections" speed={100} />
-                </>
-                <>
-                    <Test name="repeat_sky" speed={0} />
-                    <Test name="repeat_mountains_back" speed={20} />
-                    <Test name="repeat_mountains" speed={30} />
-                    <Test name="repeat_forest" speed={75} />
-                    <Test name="repeat_land" speed={85} />
-                    <Test name="repeat_water" speed={100} />
-                </>
+        <main>
+            <section className="h-[1000px] relative flex justify-center items-center">
+                <h1 className="text-3xl sm:text-4xl lg:text-6xl text-center relative bottom-40 z-10">
+                    Let's dive together <br />
+                    into <span className="uppercase">web&nbsp;development</span>
+                </h1>
+                <div className="h-full w-full overflow-hidden absolute top-0">
+                    <>
+                        <RepeatTest name="repeat_sky" speed={0} />
+                        <RepeatTest name="repeat_mountains_back" speed={20} />
+                        <RepeatTest name="repeat_mountains" speed={30} />
+                        <RepeatTest name="repeat_forest" speed={75} />
+                        <RepeatTest name="repeat_land" speed={85} />
+                        <RepeatTest name="repeat_water" speed={100} />
+                    </>
+                    <>
+                        <Test name="sky" speed={0} />
+                        <Test name="sun" speed={0} />
+                        <Test name="mountains_back" speed={20} />
+                        <Test name="mountains" speed={30} />
+                        <Test name="forest" speed={75} />
+                        <Test name="land" speed={85} />
+                        <Test name="water" speed={100} />
+                        <Test name="boat" speed={100} />
+                        <Test name="reflections" speed={100} />
+                    </>
+                </div>
             </section>
             <section className="w-full h-[4000px] relative z-10 bg-red-500"></section>
         </main>
@@ -64,12 +70,44 @@ const Test = (props: TestProps) => {
         <Image
             src={`/images/parallax/${name}.svg`}
             alt="Background"
-            className={`h-[1000px] ${position} top-0 max-w-none left-1/2 -translate-x-[50%]`}
+            className={`h-[1000px] ${position} top-0 max-w-none left-1/2 -translate-x-1/2`}
             width={3000}
             height={1000}
             ref={ref}
+            priority={true}
         />
     );
 };
 
+const RepeatTest = (props: TestProps) => {
+    const { name, speed } = props;
+
+    const ref = useRef<HTMLDivElement>(null);
+    const position = speed === 100 ? "absolute" : "fixed";
+
+    useEffect(() => {
+        if (speed === 100) return;
+
+        const listener = () => {
+            if (!ref.current) return;
+            ref.current.style.transform = `translateY(${
+                -window.scrollY * (speed / 100)
+            }px)`;
+        };
+
+        window.addEventListener("scroll", listener);
+
+        return () => {
+            window.removeEventListener("scroll", listener);
+        };
+    }, []);
+
+    return (
+        <div
+            style={{ backgroundImage: `url(/images/parallax/${name}.svg)` }}
+            className={`h-[1000px] w-full ${position} top-0 bg-center`}
+            ref={ref}
+        />
+    );
+};
 export default HomePage;
